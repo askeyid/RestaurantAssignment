18129df3abc60c3094f8400eb2429f974fc69006
Aleksandr Malygon <a_m_style@outlook.com>
2024-05-03 15:41:03 +0300

    add CheckoutSystem

---
+++ Services/CheckoutSystem.cs
@@ -0,0 +1,26 @@
+﻿namespace RestaurantAssignment.Services
+{
+    public class CheckoutSystem
+    {
+        private Menu _menu;
+        private OrderManager _orderManager;
+
+        public CheckoutSystem(Menu menu, OrderManager orderManager)
+        {
+            _menu = menu;
+            _orderManager = orderManager;
+        }
+
+        // Calculating the total bill for the order.
+        public decimal CalculateTotalBill()
+        {
+            return _orderManager.CalculateTotalBill();
+        }
+
+        // Adding an item to the order.
+        public void AddItemToOrder(MenuItemType itemType)
+        {
+            _orderManager.AddItemToOrder(itemType, _menu);
+        }
+    }
+}
