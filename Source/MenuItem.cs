e2dc2684e2815d1ae1756c99e8d664fc363cd118
Aleksandr Malygon <a_m_style@outlook.com>
2024-05-03 14:12:09 +0300

    add Menu

---
+++ Source/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿namespace RestaurantAssignment.Source
+{
+    public enum MenuItemType
+    {
+        Starter,
+        Main,
+        Drink
+    }
+
+    public class MenuItem
+    {
+        public MenuItemType Name { get; }
+        public decimal Price { get; }
+
+        public MenuItem(MenuItemType name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
